f444a5f0677b866e39b15711ff55cada91fa5999
Aleksi Torhamo <aleksi@torhamo.net>
2012-03-02 11:11:57 +0200

    Parametrized pitch and notch size

--- gen/pinhdr.py
+++ gen/pinhdr.py
@@ -7,12 +7,20 @@ import kicad
 #pad_shape = 'oval'
 #pad_size = (600, 1200)
 #drill_size = 400
+#notch = 250
 
 # Oval pads don't fit if there's more than 1 row
 pad_shape = 'circle'
 pad_size = (600, 600)
 # For a square 0.025" pin, diameter is a bit more than 0.035"
 drill_size = 400
+notch = 200
+
+# If you change the pitch, remember to change:
+# - The basename
+# - The documentation string
+basename = 'PINHDR'
+pitch = 1000
 
 def new_pad(position, name):
 	# Variable: pad.name, pad.position
@@ -56,12 +64,13 @@ def new_segment(p1, p2):
 def new_module(cols, rows = 1):
 	rowstr = {1: 'single', 2: 'double'}.get(rows, str(rows))
 	short = 'MA%02d-%d' % (cols, rows)
-	name = 'PINHDR-' + short
+	name = basename + '-' + short
 
-	xbase, ybase = -500 * (cols - 1), -500 * (rows - 1)
-	minx, miny = xbase - 500, ybase - 500
-	maxx, maxy = xbase + (cols - 1) * 1000 + 500, ybase + (rows - 1) * 1000 + 500
-	minl, maxl = miny + 250, maxy - 250
+	pitch2 = pitch/2
+	xbase, ybase = -pitch2 * (cols - 1), -pitch2 * (rows - 1)
+	minx, miny = xbase - pitch2, ybase - pitch2
+	maxx, maxy = xbase + (cols - 1) * pitch + pitch2, ybase + (rows - 1) * pitch + pitch2
+	minl, maxl = miny + notch, maxy - notch
 
 	mod = kicad.Module()
 	mod.name = name
@@ -77,8 +86,8 @@ def new_module(cols, rows = 1):
 	mod.path = ''
 	mod.r90 = mod.r180 = mod.xxx1 = 0
 	mod.attrs = []
-	mod.reference = new_texte('reference', (0, miny - 500), '"%s"' % (short, ))
-	mod.value = new_texte('value', (0, maxy + 500), '"VAL**"')
+	mod.reference = new_texte('reference', (0, miny - pitch2), '"%s"' % (short, ))
+	mod.value = new_texte('value', (0, maxy + pitch2), '"VAL**"')
 	mod.text2 = None
 	mod.draws = []
 	mod.pads = []
@@ -86,20 +95,20 @@ def new_module(cols, rows = 1):
 
 	for row in range(rows):
 		for col in range(cols):
-			pad_pos = (xbase + col * 1000, ybase + row * 1000)
+			pad_pos = (xbase + col * pitch, ybase + row * pitch)
 			pad_name = '"%d"' % (col * rows + (rows - row), )
 			mod.pads.append(new_pad(pad_pos, pad_name))
 
 	mod.draws.append(new_segment((minx, minl), (minx, maxl)))
 	for col in range(cols):
-		base = minx + col * 1000
+		base = minx + col * pitch
 		mod.draws.extend((
-			new_segment((base, minl), (base + 250, miny)),
-			new_segment((base, maxl), (base + 250, maxy)),
-			new_segment((base + 250, miny), (base + 750, miny)),
-			new_segment((base + 250, maxy), (base + 750, maxy)),
-			new_segment((base + 750, miny), (base + 1000, minl)),
-			new_segment((base + 750, maxy), (base + 1000, maxl)),
+			new_segment((base, minl), (base + notch, miny)),
+			new_segment((base, maxl), (base + notch, maxy)),
+			new_segment((base + notch, miny), (base + (pitch - notch), miny)),
+			new_segment((base + notch, maxy), (base + (pitch - notch), maxy)),
+			new_segment((base + (pitch - notch), miny), (base + pitch, minl)),
+			new_segment((base + (pitch - notch), maxy), (base + pitch, maxl)),
 		))
 	mod.draws.append(new_segment((maxx, minl), (maxx, maxl)))
 

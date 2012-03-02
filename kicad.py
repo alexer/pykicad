4fb884f2c2c1ee67a9c862baf2cabf9e14741fc9
Aleksi Torhamo <aleksi@torhamo.net>
2012-03-02 12:40:18 +0200

    Moved dumper to kicaddump.py

--- kicad.py
+++ kicad.py
@@ -566,24 +566,3 @@ def load_mod(f):
 	assert line == ''
 	return result
 
-loaders = {
-	'mod': load_mod,
-}
-
-if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print 'usage: %s FILE' % (sys.argv[0], )
-		sys.exit(1)
-
-	fname = sys.argv[1]
-	ext = os.path.splitext(fname)[1][1:]
-	loader = loaders.get(ext)
-
-	if not loader:
-		print 'Unknown file extension! Should be one of:', ', '.join('.' + ext for ext in loaders)
-		sys.exit(1)
-
-	with file(fname) as f:
-		result = loader(f)
-		result.dump()
-
---
+++ kicaddump.py
@@ -0,0 +1,24 @@
+import sys, os
+import kicad
+
+loaders = {
+	'mod': kicad.load_mod,
+}
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print 'usage: %s FILE' % (sys.argv[0], )
+		sys.exit(1)
+
+	fname = sys.argv[1]
+	ext = os.path.splitext(fname)[1][1:]
+	loader = loaders.get(ext)
+
+	if not loader:
+		print 'Unknown file extension! Should be one of:', ', '.join('.' + ext for ext in loaders)
+		sys.exit(1)
+
+	with file(fname) as f:
+		result = loader(f)
+		result.dump()
+
